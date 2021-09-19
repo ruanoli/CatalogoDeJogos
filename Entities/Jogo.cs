024b0a7f9d0279e11fadb3fd0950bd0e4224d9f5
Ruan Oliveira <ruanoliveira.pro@gmail.com>
2021-09-19 16:41:13 -0300

    First commit

---
+++ Entities/Jogo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExemploApiCatalogoJogos.Entities
+{
+    //Elementos necessários para cadastro de jogos
+    public class Jogo
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Produtora { get; set; }
+        public double Preco { get; set; }
+    }
+}
